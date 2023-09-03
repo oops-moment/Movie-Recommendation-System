8f3b0e5266a2203d494f567c287f69390411824a
Prisha <prisha.kumar@students.iiit.ac.in>
2023-09-04 01:48:56 +0530

    Update app.py
    final

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
+import pickle
 import streamlit as st
-st.title('Movie Recommender System')
 
-# we need text box where user will type
-option = st.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
 
-st.write('You selected:', option)
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(
+        list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = i[0]
+
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names
+
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movies_dict.pkl', 'rb'))
+similarity = pickle.load(open('similiarity.pkl', 'rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names = recommend(
+        selected_movie)
+
+    for i in recommended_movie_names:
+        st.write(i)
